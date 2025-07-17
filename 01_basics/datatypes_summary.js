dd865ac3317e6f0e0778edaf6aea6ff640f9f995
Mohammad Affan Siddiqi <mohammadaffansiddiqi98@gmail.com>
2025-07-17 19:25:09 +0530

    Memory Allocation Studied

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -29,3 +29,27 @@ funcVar2()
 
 /*With or without a variable, if making & using func in JS ALWAYS have to
 use function keyword or => notation*/
+
+// Memory Allocation
+// Stack (For Primitive)     & Heap (For Non Primitive)
+// Copy is made address same | Two diff addresses point to same value in heap
+let UID="anonymous@domain.com"
+let UID_Dup=UID
+console.log("Before:\n")
+console.table([UID,UID_Dup])
+UID_Dup="yourname@domain.com"
+console.log("After:\n")
+console.table([UID,UID_Dup]) //only UID_Dup gets affected here
+
+let userData=
+{
+    user_name:"Affan",
+    userID:"affan240@domain.com",
+    city:"New Delhi"
+}
+let userRef=userData
+console.log('Before\n')
+console.table([userData,userRef])
+userRef.userID="notfound@broken.com"
+console.log("\n\nAfter\n")
+console.table([userData,userRef]) //Both object fields get affected
